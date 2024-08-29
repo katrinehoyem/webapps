3184cfbb7eddfa7d80daeff0a0e6c6d78d78ea49
Katrine Høyem <katrinhh@hiof.no>
2024-08-29 13:25:45 +0200

    added server

---
+++ forelesning-2/server.ts
@@ -0,0 +1,8 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+const app = new Hono()
+
+app.use("/*", cors());
