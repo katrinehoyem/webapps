d650a2f124af620690b421edf9a566ac1b7010c3
Katrine Høyem <katrinhh@hiof.no>
2024-08-29 13:29:41 +0200

    Update server.ts

--- forelesning-2/server.ts
+++ forelesning-2/server.ts
@@ -6,3 +6,18 @@ import { serveStatic } from "@hono/node-server/serve-static";
 const app = new Hono()
 
 app.use("/*", cors());
+
+app.use("/statics/*", serveStatic({root: "./"}));
+
+app.get('/json', async (c) => {
+    return c.json({})
+});
+
+const port = 3999;
+
+console.log("Server is running, fuck ye")
+
+serve({
+    fetch: app.fetch,
+    port,
+});
