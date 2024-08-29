34a0245b4e9ef09cffe5344fe99c656bbe6cd82c
Katrine Høyem <katrinhh@hiof.no>
2024-08-29 13:44:51 +0200

    Update main.ts

--- forelesning-2/main.ts
+++ forelesning-2/main.ts
@@ -1,1 +1,12 @@
 import './style.css'
+
+const fetchDataFromServer = async () => {
+  const respons = await fetch('http://localhost:3999/json')
+  const result = await respons.json()
+
+  const id = document.getElementById("json");
+  id?.innerHTML = result;
+
+}
+
+fetchDataFromServer();
