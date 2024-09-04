9ad330c8c4412e9c73b0bdd66b54da142f64e2fc
Katrine Høyem <katrinhh@hiof.no>
2024-09-04 18:35:48 +0200

    masse

--- portefølje/script.js
+++ portefølje/script.js
@@ -1,10 +1,36 @@
-function saveProject (){
-    let projects = [];
+//function saveProject (){
+  //  let projects = [];
 
-    const projectInput = document.querySelectorAll("#project-list input");
+    //const projectInput = document.querySelectorAll("#project-list input");
 
-    for (const element of projectInput) {
-        projects.push(element.textContent);
-    }
-    localStorage.setItem("projects.textContent");
-}
+    //for (const element of projectInput) {
+      //  projects.push(element.textContent);
+    //}
+    //localStorage.setItem("projects.textContent");
+//}
+
+const form = document.getElementById('myProjects');
+const projects= [];
+
+form.addEventListener('add-project-button', function(event){
+    event.preventDefault();
+
+    const formData = {};
+    new FormData(form).forEach((value, key) => {
+        formData[key] = value;
+    });
+
+
+    const title = formData.title;
+    const link = formData.link;
+    const description = formData.description;
+   console.log(`title: ${title}, link: ${link}, description: ${description}`);
+   const newProject ={
+    title: title, link: link, description: description
+    };
+
+    projects.push(newProject);
+    
+
+});
+
