e245b113f51477b3223b6555b4d494dbb294fd94
Katrine Høyem <katrinhh@hiof.no>
2024-09-05 12:17:35 +0200

    Update main.ts

--- forelesning-3/frontend-ts/src/main.ts
+++ forelesning-3/frontend-ts/src/main.ts
@@ -14,4 +14,26 @@ const loadWeatherData = async() => {
 
     const data = await response.json()
     console.log(data.data)
-}
+};
+
+const addWeatherData = async (weather: unknown) => {
+    try {
+      const response = await fetch("http://localhost:3999", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(weather),
+      });
+  
+      console.log(response.status);
+      console.log(response.ok);
+      const data = await response.json();
+      console.log(data);
+      loadWeatherData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+loadWeatherData();
