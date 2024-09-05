308c62c99adb6b0ea36adc22f8d6f211dc20f121
Katrine Høyem <katrinhh@hiof.no>
2024-09-05 11:31:58 +0200

    live kodeing

--- forelesning-3/backend/src/lib.ts
+++ forelesning-3/backend/src/lib.ts
@@ -1,5 +1,4 @@
 import { readFile } from "fs/promises";
-import { text } from "stream/consumers";
 
 export async function getWeatherData() {
     const data = await readFile('./data/weather.json', "utf-8");
--- forelesning-3/frontend-ts/src/main.ts
+++ forelesning-3/frontend-ts/src/main.ts
@@ -1,1 +1,17 @@
 import "./style.css";
+
+const loadWeatherData = async() => {
+    const response = await fetch('http://localhost:3999', {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }
+        
+    );
+    console.log(response.status)
+    console.log(response.ok)
+
+    const data = await response.json()
+    console.log(data.data)
+}
