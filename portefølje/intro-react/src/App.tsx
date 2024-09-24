60a3920758b547536091dca4c8288d95f52b0cec
Katrine Høyem <katrinhh@hiof.no>
2024-09-24 19:17:55 +0200

    kuk

--- portefølje/intro-react/src/App.tsx
+++ portefølje/intro-react/src/App.tsx
@@ -3,12 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
+  const student = 'Halgeir Geirson'
+  const degree = 'Bachelor IT'
+  const points = 180
+  const experienceOne = 'Figma UI for customer X'
+  const experienceTwo = 'Website for customer Y'
+  const email = 'student@hiof.no'
+
   return (
-    <main>
-      <h1>Velkommen til streaky</h1>
-    </main>
-  );
+    <div>
+      <Header student={student} degree={degree} points={points} />
+      <Experiences experienceOne={experienceOne} experienceTwo={experienceTwo} />
+      <Contact email={email} />
+    </div>
+  )
 }
 
-export default App;
+export default App
+export default function Experiences({ experienceOne, experienceTwo }) {
+  return (
+    <div>
+      <Experience description={experienceOne} />
+      <Experience description={experienceTwo} />
+    </div>
+  )
+}
