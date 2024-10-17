cc8b5b52bb3e4e4188cc71e18cb7f89348eae303
Katrine Høyem <katrinhh@hiof.no>
2024-10-17 12:17:15 +0200

    Update app.ts

--- forelesning-8/backend/src/app.ts
+++ forelesning-8/backend/src/app.ts
@@ -5,6 +5,48 @@ const app = new Hono();
 
 app.use("/*", cors());
 
+ let students = [
+  { id: "1", name: "Ola Normann" },
+  { id: "2", name: "Kari Normann" },
+];
+
+app.get('/api/students', (c) => {
+  return c.json(students);
+
+});
+
+app.get('/api/students/:id', (c) => {
+  const id = c.req.param ("id");
+  const student = students.filter((student)=> student.id === id)
+  return c.json(students);
+
+});
+
+app.post('/api/students', async(c) => {
+  const data = await c.req.json ();
+  const {name} = data;
+  students.push({id: crypto.randomUUID(), name});
+  return c.json(students, {status: 201});
+
+});
+
+app.delete('/api/students/:id', (c) => {
+  const id = c.req.param ("id");
+  const removedStudentFromList = students.filter(
+    (student)=> student.id !== id);
+  return c.json(students);
+
+});
+
+app.patch('/api/students/:id', async (c) => {
+  const id = c.req.param ("id");
+  const {name} = await c.req.json();
+  students = students.map((student) => 
+    student.id == id? {...student,name} : student);
+  return c.json(students);
+
+});
+
 app.onError((err, c) => {
   console.error(err);
 
